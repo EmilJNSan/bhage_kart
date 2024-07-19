134810decd280e76b83251edd181cb30a8303c7a
EmilJNSan <Emil.Johan.Nyfors@Sandnes.Kommune.no>
2024-07-19 11:47:03 +0200

    Update variables_bhage.js

--- js/variables_bhage.js
+++ js/variables_bhage.js
@@ -7,43 +7,42 @@ const marker_blaa = "css/images/icon/marker-icon_blaa.png";
 const marker_gron = "css/images/icon/marker-icon_gron.png";
 const shadow = "css/images/marker-shadow.png";
 const testtekst = "test ok";
+var publicCluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
 
-        var publicCluster = L.markerClusterGroup({
-            maxClusterRadius: 55,
-            disableClusteringAtZoom: 16
-        });
+var privateCluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
 
-        var privateCluster = L.markerClusterGroup({
-            maxClusterRadius: 55,
-            disableClusteringAtZoom: 16
-        });
+var openCluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
+var allcluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
 
-        var openCluster = L.markerClusterGroup({
-            maxClusterRadius: 55,
-            disableClusteringAtZoom: 16
-        });
-        var allcluster = L.markerClusterGroup({
-            maxClusterRadius: 55,
-            disableClusteringAtZoom: 16
-        });
+var blaatikon = L.icon({
+    iconUrl: `${direct}${marker_blaa}`,
+    shadowUrl: `${direct}${shadow}`,
+    iconSize: [25, 41],
+    shadowSize: [25, 41]
+});
 
-        var blaatikon = L.icon({
-            iconUrl: `${direct}${marker_blaa}`,
-            shadowUrl: `${direct}${shadow}`,
-            iconSize: [25, 41],
-            shadowSize: [25, 41]
-        });
+var gronikon = L.icon({
+    iconUrl: `${direct}${marker_gron}`,
+    shadowUrl: `${direct}${shadow}`,
+    iconSize: [25, 41],
+    shadowSize: [25, 41]
+});
 
-        var gronikon = L.icon({
-            iconUrl: `${direct}${marker_gron}`,
-            shadowUrl: `${direct}${shadow}`,
-            iconSize: [25, 41],
-            shadowSize: [25, 41]
-        });
-
-        var oransjeikon = L.icon({
-            iconUrl: `${direct}${marker_oransje}`,
-            shadowUrl: `${direct}${shadow}`,
-            iconSize: [25, 41],
-            shadowSize: [25, 41]
-        });
+var oransjeikon = L.icon({
+    iconUrl: `${direct}${marker_oransje}`,
+    shadowUrl: `${direct}${shadow}`,
+    iconSize: [25, 41],
+    shadowSize: [25, 41]
+});
