72f25c89a52976b8d6fefc51e3f959f4c7410420
EmilJNSan <Emil.Johan.Nyfors@Sandnes.Kommune.no>
2024-07-19 09:15:06 +0200

    Create variables_akt.js

---
+++ js/variables_akt.js
@@ -0,0 +1,105 @@
+const jsonUrl = "https://Laila72.github.io/Park_data/aktiviteter.json";
+const direct = "https://emiljnsan.github.io/bhage_kart/";
+const grunnkart = "https://services.geodataonline.no/arcgis/rest/services/Geocache_WMAS_WGS84/GeocacheBasis/MapServer/tile/{z}/{y}/{x}";
+const attribution_tag = '©️ Geodata AS, Kartverket, Geovekst og kommunene, OpenStreetMap';
+const marker_blaa = "https://emiljnsan.github.io/bhage_kart/icon/marker-icon_park_blaa.png";
+const testtekst = "Test variabler variant 1"
+
+
+
+var group_all_Cluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
+
+var group_annet_Cluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
+
+var group_100_Cluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
+
+var group_101_Cluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
+
+var group_102_Cluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
+
+var group_907_Cluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
+
+var group_201_Cluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
+
+var group_300_Cluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
+
+var group_301_Cluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
+
+var group_902_Cluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
+
+var group_908_Cluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
+
+var group_900_Cluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
+
+var group_901_Cluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
+
+var group_903_Cluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
+
+var group_906_Cluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
+
+var group_904_Cluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
+
+var group_911_Cluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
+
+var group_905_Cluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
+
+var group_909_Cluster = L.markerClusterGroup({
+    maxClusterRadius: 55,
+    disableClusteringAtZoom: 16
+});
+
+
