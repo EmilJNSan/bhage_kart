29fe7763082b57a3cd1100685836a06b535e1998
EmilJNSan <Emil.Johan.Nyfors@Sandnes.Kommune.no>
2024-07-19 10:11:58 +0200

    Update variables_bhage.js

--- js/variables_bhage.js
+++ js/variables_bhage.js
@@ -1,8 +1,8 @@
 const jsonUrl = "https://Laila72.github.io/Park_data/bhg.json";
-const direct = "/EPiServer/CMS/Content/globalassets/webredaktor/barnehage-kart/";
+const direct = "https://emiljnsan.github.io/bhage_kart/;
 const grunnkart = "https://services.geodataonline.no/arcgis/rest/services/Geocache_WMAS_WGS84/GeocacheBasis/MapServer/tile/{z}/{y}/{x}";
 const attribution_tag = '©️ Geodata AS, Kartverket, Geovekst og kommunene, OpenStreetMap';
-const marker_oransje = "marker-icon_park_oransje.png";
+const marker_oransje = "images/icon/marker-icon_park_oransje.png";
 const marker_blaa = "marker-icon_park_blaa.png";
 const marker_gron = "marker-icon_park_gron.png";
 const shadow = "marker-shadow.png";
